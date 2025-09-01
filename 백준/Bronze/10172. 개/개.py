fab938a7d1a4d38a34318405cca3256b43599127
JeonSeokMin <156174027+JeonSeokMin@users.noreply.github.com>
2025-09-01 20:02:56 +0900

    [Bronze V] Title: 개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.py
@@ -0,0 +1,11 @@
+first_line = "|\_/|"
+second_line = "|q p|   /}"
+third_line = "( 0 )\"\"\"\\"
+fourth_line = "|\"^\"`    |"
+last_line = "||_/=\\\__|"
+
+print(first_line)
+print(second_line)
+print(third_line)
+print(fourth_line)
+print(last_line)
